be7216f78c45eafd4b9512044e9e5bee7ec1b218
滕瀚斯 <hansjason@126.com>
2014-09-20 21:54:27 +0800

    pointcut

--- src/com/hans/aop/LogInterceptor.java
+++ src/com/hans/aop/LogInterceptor.java
@@ -3,18 +3,22 @@ package com.hans.aop;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogInterceptor {
+
+	@Pointcut("execution(* save(..))")
+	public void anyOldTransfer() {}
 	
-	@Before("execution(* com.hans.dao.impl.*.*(*))")
+	@Before("anyOldTransfer()")
 	public void before() {
 		System.out.println("method start...");
 	}
 	
-	@After("execution(int com.hans.dao.impl.UserDaoImpl.save(com.hans.model.User))")
+	@After("anyOldTransfer()")
 	public void after() {
 		System.out.println("method end .....");
 	}
